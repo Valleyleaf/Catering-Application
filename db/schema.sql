0be998233245750f3cd1035b42aa01365cc1221a
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-30 19:48:36 -0300

    Adding additional init files to be used. Did some baseline SQL

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS catering_db;
+CREATE DATABASE catering_db;
+
+USE catering_db;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30) NOT NULL
+  user_password VARCHAR(30) NOT NULL
+
+);
+
+CREATE TABLE resturants (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  resturant_name VARCHAR(30) NOT NULL,
+  speciality VARCHAR(15) NOT NULL,
+);

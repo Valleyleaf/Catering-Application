d680bfc43b129492047afdfb225f36d9a6592144
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-11-01 21:31:41 -0300

    Added structure and psudocoded handlebar files. Minor changes to main files

--- server.js
+++ server.js
@@ -2,24 +2,35 @@
 const express = require('express');
 const eHandleBars = require('express-handlebars');
 const path = require('path');
+const session = require('express-session');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const PLACEHOLDER = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 const hbs = eHandleBars.create({});
 // ------------------------------
 
+app.use(session(sess));
+// Sets up user session and stores local memory.
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+// Above is just the engine declarator for Handebars. It does not impact anything
+// Outside this document.
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// app being express, this sets out static directory return. We may have to change this down the line to ensure 
+// we hit main.handlebars
 
 app.use(require('./controllers/'));
-
+// This just sets up our routers via controllers.
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
+//Sequelize above sets up our db via connection in config. Ask Toua about JAWSDB_URL located in there.

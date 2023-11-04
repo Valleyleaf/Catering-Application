153ca2c958670abcbdff11ac12c783aba07944ed
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2023-11-04 17:10:07 -0300

    the main handlebars header has been updated, switched the index controller to render the homepage first, not the body

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
 const main = '../../views/layouts/main.handlebars'
 
-const test = (req, res, next) => {
-    res.render('body');
+const test = (req, res, next) => {  //I did it ths way for now to work on the handlebars, will configure it later to use homeroutes -JKD
+    res.render('homepage');
 }
  
 router.use('/', test);  // This allows us to use our homeroutes js script  -JKD

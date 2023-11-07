6ecea2042e9aeedcfb3348429e58f00f90f4582f
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-11-06 21:31:06 -0400

    Accidentally doxxed myself. fixing

--- config/connection.js
+++ config/connection.js
@@ -7,12 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
-    "catering_db",
-    "root",
-    "Ayxhee2n",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',

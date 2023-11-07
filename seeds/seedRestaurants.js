ca4acc276b499645bdc5a06f3b5835f6b596d857
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2023-11-07 19:07:56 -0400

    Update seedRestaurants.js
    Branch looks good but seedRestaurants got deleted or something, so I am re-adding that and then Commiting changes.

--- seeds/seedRestaurants.js
+++ seeds/seedRestaurants.js
@@ -1,4 +1,52 @@
-
+const sequelize = require('../config/connection');
+const {Restaurants,MenuItem} = require('../models');
+const seedData = [
+    {
+        name: 'The Italiano',
+        cuisine: 'Italian',
+        menuItems: [
+          'Spaghetti Carbonara',
+          'Margherita Pizza',
+          'Chicken Alfredo',
+        ],
+      },
+      {
+        name: 'Greek Vacation',
+        cuisine: 'Greek',
+        menuItems: [
+          'Gyros with Tzatziki',
+          'Moussaka',
+          'Greek Salad',
+        ],
+      },
+      {
+        name: 'Classic American',
+        cuisine: 'American',
+        menuItems: [
+          'Clam Chowder',
+          'Philly Cheesesteak',
+          'Chocolate Milkshake',
+        ],
+      },
+      {
+        name: 'Tastes of the East',
+        cuisine: 'Japanese',
+        menuItems: [
+          'Sushi Platter',
+          'Ramen Noodle Soup',
+        ],
+      },
+      {
+        name: 'Mexican package',
+        cuisine: 'Mexican',
+        menuItems: [
+          'Tacos (Choice of Meat)',
+          'Enchiladas',
+          'Guacamole and Chips',
+          'Burrito Bowl',
+        ],
+      },
+    ];
     const seedRestaurants = async () => {
       try {
         await sequelize.sync({ force: true }); // Sync the models with the database
